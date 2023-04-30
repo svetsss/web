74ed4a1de2a21401e8e6a2e59836a65d9dbf4a5d
svetsss <92213711+svetsss@users.noreply.github.com>
2023-04-30 22:26:40 +0500

    Add files via upload

---
+++ cookie_test.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    app.run()
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session_test.get('visits_count', 0)
+    session_test['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+if __name__ == '__main__':
+    main()
